509958ca841e7151396b2498f1e9e3b86d0f1960
Redwood-Jia <786783037@qq.com>
2023-08-21 00:56:25 +0800

    ready to submit my lab

--- kernel/proc.c
+++ kernel/proc.c
@@ -313,6 +313,9 @@ fork(void)
 
   acquire(&np->lock);
   np->state = RUNNABLE;
+
+   //copy mask
+  np->mask = p->mask;
   release(&np->lock);
 
   return pid;
@@ -653,4 +656,28 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+// Return the number of processes whose state is not UNUSED
+uint64
+nproc(void)
+{
+  struct proc *p;
+  // counting the number of processes
+  uint64 num = 0;
+  // traverse all processes
+  for (p = proc; p < &proc[NPROC]; p++)
+  {
+    // add lock
+    acquire(&p->lock);
+    // if the processes's state is not UNUSED
+    if (p->state != UNUSED)
+    {
+      // the num add one
+      num++;
+    }
+    // release lock
+    release(&p->lock);
+  }
+  return num;
 }
