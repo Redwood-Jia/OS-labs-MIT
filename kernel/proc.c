28fff88a4ca8172b5f9c9e4648ec11e7f4275090
Redwood-Jia <786783037@qq.com>
2023-08-22 09:52:22 +0800

    ready to commit muy lab

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,14 @@ found:
     return 0;
   }
 
+  if ((p->usyscall = (struct usyscall *)kalloc()) == 0) {
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+
+  memmove(p->usyscall,&p->pid,8);
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -153,6 +161,8 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  if(p->usyscall)
+    kfree((void*)p->usyscall);
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -168,6 +178,9 @@ freeproc(struct proc *p)
 
 // Create a user page table for a given process,
 // with no user memory, but with trampoline pages.
+
+extern pagetable_t kernel_pagetable;
+
 pagetable_t
 proc_pagetable(struct proc *p)
 {
@@ -196,6 +209,16 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map the USYSCALL just below TRAPFRAME.
+  if(mappages(pagetable, USYSCALL, PGSIZE,
+               (uint64)(p->usyscall), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+               }
+
+
   return pagetable;
 }
 
@@ -206,7 +229,8 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
-  uvmfree(pagetable, sz);
+  uvmunmap(pagetable,USYSCALL,1,0);
+  uvmfree(pagetable,sz);
 }
 
 // a user program that calls exec("/init")
